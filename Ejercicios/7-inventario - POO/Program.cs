f0336b7b5c8637e01c77cc272eee0e9107749200
Olvin Romero <olvinromero24@gmail.com>
2021-03-07 16:29:11 -0600

    agregando inventario-poo

---
+++ Ejercicios/7-inventario - POO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace inventario
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            string opcion = "";
+            Inventario inventario = new Inventario();
+
+            while (true)
+            {
+               
+                Console.Clear();
+                Console.WriteLine("Sistema de Inventario");
+                Console.WriteLine("*********************");
+                Console.WriteLine("");
+                
+                Console.WriteLine("1 - Productos");
+                Console.WriteLine("2 - Ingreso de Inventario");
+                Console.WriteLine("3 - Salida de Inventario");
+                Console.WriteLine("4 - Ajuste positivo de inventario");
+                Console.WriteLine("5 - Ajuste negativo de inventario");
+                Console.WriteLine("0 - Salir");
+                opcion = Console.ReadLine();
+
+
+                switch (opcion)
+                {
+                    case "1": 
+                        inventario.listarProductos();
+                        break;
+                    case "2":
+                        inventario.ingresoDeInventario();
+                        break;
+                    case "3":
+                        inventario.salidaDeInventario();    
+                        break;
+                    case "4":
+                        inventario.ajustePositivoDeInventario();    
+                        break; 
+                    case "5":
+                        inventario.ajusteNegativoDeInventario();    
+                        break;                       
+                    default:
+                    break;
+                }
+
+                if (opcion == "0") {
+                    break;
+                }
+            }            
+        }
+    }
+}
